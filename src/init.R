2170580178a98a82aa28bd2afbcfdb28887ed01f
oscararturo.bringas <oscararturo.bringas@bbva.com>
2025-03-01 08:54:42 -0600

    environment update

--- src/init.R
+++ src/init.R
@@ -1,27 +1,26 @@
 library(reticulate)
 
 install_python(list = TRUE)
-install_python(version = "3.10:latest", list = TRUE)
+install_python(version = "3.11.8")
 ####################
 #### VirtualEnv ####
 ####################
 
 # create a new environment 
-use_python("/usr/bin/python3.10")
+use_python("/usr/bin/python3.11.8")
 
-virtualenv_create(envname = "venv_dsml_py3_10", python_version = "python3.10")
-virtualenv_create(envname = "venv_dsml_py3_11", python_version = "python3.11")
-virtualenv_create(envname = "venv_merama", python_version = "python3.9.1")
+# virtualenv_create(envname = "venv_dsml_py3_10", python_version = "python3.10")
+virtualenv_create(envname = "venv_dsml_py3_11", python_version = "python3.11.8")
 
 virtualenv_list()
 
-use_python("/Users/mi20447/.virtualenvs/venv_dsml_py3_10/bin/python")
-use_virtualenv("venv_dsml_py3_10")
+use_virtualenv("venv_dsml_py3_11")
 
 # import pandas 
 pandas <- import("pandas")
 
 # install pandas
+virtualenv_install("venv_dsml_py3_11", requirements = "data/requirements.txt")
 virtualenv_install("venv_dsml_py3_10", "numpy")
 virtualenv_install("venv_dsml_py3_10", "pandas")
 virtualenv_install("venv_dsml_py3_10", "openpyxl")
